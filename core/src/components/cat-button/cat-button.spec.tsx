0c4fe893624e3ab405c13654079066924dca93c2
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2022-01-26 15:38:17 +0100

    fix some tests

--- core/src/components/cat-button/cat-button.spec.tsx
+++ core/src/components/cat-button/cat-button.spec.tsx
@@ -10,7 +10,11 @@ describe('cat-button', () => {
     expect(page.root).toEqualHtml(`
       <cat-button>
         <mock:shadow-root>
-          <slot></slot>
+          <button class="cat-button cat-button-ellipsed cat-button-filled cat-button-m cat-button-secondary" part="button" type="button">
+            <span class="cat-button-content" part="content">
+              <slot></slot>
+            </span>
+          </button>
         </mock:shadow-root>
       </cat-button>
     `);
--- core/src/components/cat-icon/cat-icon.spec.tsx
+++ core/src/components/cat-icon/cat-icon.spec.tsx
@@ -1,8 +1,14 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { CatIcon } from './cat-icon';
+import { CatIconRegistry } from './cat-icon-registry';
+
+jest.mock('./cat-icon-registry')
 
 describe('cat-icon', () => {
   it('renders', async () => {
+    CatIconRegistry.getInstance = jest.fn().mockReturnValue({
+      getIcon: () => 'ssfd'
+    });
     const page = await newSpecPage({
       components: [CatIcon],
       html: `<cat-icon></cat-icon>`
