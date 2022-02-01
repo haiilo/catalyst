cd6422cef02d2c5cc50ce55a97e51e223e1e49a8
fynnfeldpausch <fynnfeldpausch@users.noreply.github.com>
2022-02-01 09:59:57 +0000

    Update tokens

---
+++ tokens/js/variables.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 01 Feb 2022 09:59:57 GMT
+ */
+
+export const SizingXs : string;
+export const SizingS : string;
+export const SizingM : string;
+export const SizingL : string;
+export const SizingXl : string;
+export const SpacingXs : string;
+export const SpacingS : string;
+export const SpacingM : string;
+export const SpacingL : string;
+export const SpacingXl : string;
---
+++ tokens/js/variables.js
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 01 Feb 2022 09:59:57 GMT
+ */
+
+export const SizingXs = "1.5rem";
+export const SizingS = "2rem";
+export const SizingM = "2.5rem";
+export const SizingL = "3rem";
+export const SizingXl = "3.5rem";
+export const SpacingXs = "0.25rem";
+export const SpacingS = "0.5rem";
+export const SpacingM = "1rem";
+export const SpacingL = "1.5rem";
+export const SpacingXl = "2rem";
