76387b96aac1a61b957b0018edf6135947bebe84
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2022-03-09 14:35:49 +0100

    chore: minor adjustments

--- tokens/src/color/ui.js
+++ tokens/src/color/ui.js
@@ -13,9 +13,9 @@ module.exports = {
         focus: { value: '#0071ff' }
       },
       font: {
-        head: { value: '{color.base.neutral.0.value}', attributes: { cssProp: "font-color-head" } },
-        body: { value: '{color.base.neutral.0.value}', attributes: { cssProp: "font-color-body" } },
-        mono: { value: '{color.base.neutral.0.value}', attributes: { cssProp: "font-color-mono" } },
+        head: { value: '{color.base.neutral.900.value}', attributes: { cssProp: "font-color-head" } },
+        body: { value: '{color.base.neutral.900.value}', attributes: { cssProp: "font-color-body" } },
+        mono: { value: '{color.base.neutral.900.value}', attributes: { cssProp: "font-color-mono" } },
         muted: { value: '{color.base.neutral.400.value}', attributes: { cssProp: "font-color-muted" } },
         quote: { value: '{color.ui.font.body.value}' },
         quoteSource: { value: '{color.ui.font.muted.value}' }
