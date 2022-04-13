73d5822774562499bd2d75e5d1d53bb58cb696d6
Manuel Manolache <manyx@gmail.com>
2022-04-13 11:16:29 +0300

    feat(core): tests

--- core/src/components/cat-scrollable/cat-scrollable.spec.tsx
+++ core/src/components/cat-scrollable/cat-scrollable.spec.tsx
@@ -5,13 +5,19 @@ describe('cat-scrollable', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [CatScrollable],
-      html: `<cat-scrollable></cat-scrollable>`,
+      html: `<cat-scrollable></cat-scrollable>`
     });
     expect(page.root).toEqualHtml(`
-      <cat-scrollable>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
+      <cat-scrollable class="scroll-x scroll-y">
+       <mock:shadow-root>
+         <div class='shadow-wrapper'>
+           <div class='shadow-top'></div>
+           <div class='shadow-left'></div>
+           <div class='shadow-right'></div>
+           <div class='shadow-bottom'></div>
+         </div>
+         <div class='scrollable-content'>
+           <slot></slot>
       </cat-scrollable>
     `);
   });
