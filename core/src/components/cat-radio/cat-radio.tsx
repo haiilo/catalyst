642cff0d7f037202c5abf65f1bf8381970bcdd69
geralrodrigues <100929087+geralrodrigues@users.noreply.github.com>
2022-04-29 10:33:05 +0100

    feat: add radio button component

---
+++ core/src/components/cat-radio/cat-radio.tsx
@@ -0,0 +1,84 @@
+import { Component, Event, EventEmitter, h, Prop } from '@stencil/core';
+import log from 'loglevel';
+
+let nextUniqueId = 0;
+
+@Component({
+  tag: 'cat-radio',
+  styleUrl: 'cat-radio.scss',
+  shadow: true
+})
+export class CatRadio {
+  private readonly id = `cat-radio-${++nextUniqueId}`;
+
+  /**
+   * Whether this radio is checked.
+   * */
+  @Prop() checked = false;
+
+  /**
+   *  Whether this radio is disabled.
+   *  */
+  @Prop() disabled = false;
+
+  /**
+   * The label of the radio that is visible.
+   */
+  @Prop() label = '';
+
+  /**
+   * Visually hide the label, but still show it to assistive technologies like screen readers.
+   */
+  @Prop() labelHidden = false;
+
+  /**
+   * The name of the radio component.
+   */
+  @Prop() name?: string;
+
+  /**
+   * Whether the radio is required.
+   * */
+  @Prop() required = false;
+
+  /**
+   * The value of the radio component.
+   */
+  @Prop() value?: string;
+
+  /**
+   * Emitted when the radio is changed.
+   */
+  @Event() catChange!: EventEmitter;
+
+  componentWillRender(): void {
+    if (!this.label) {
+      log.error('[A11y] Missing ARIA label on radio button', this);
+    }
+  }
+
+  render() {
+    return (
+      <label htmlFor={this.id} class={{ 'is-hidden': this.labelHidden, 'is-disabled': this.disabled }}>
+        <span class="checkbox">
+          <input
+            id={this.id}
+            type="radio"
+            name={this.name}
+            value={this.value}
+            checked={this.checked}
+            required={this.required}
+            disabled={this.disabled}
+            onChange={this.onChange.bind(this)}
+          />
+          <span class="circle"></span>
+        </span>
+        <span class="label">{this.label}</span>
+      </label>
+    );
+  }
+
+  private onChange(event: Event) {
+    this.catChange.emit(event);
+  }
+}
--- react/src/components/stencil-generated/index.ts
+++ react/src/components/stencil-generated/index.ts
@@ -13,6 +13,7 @@ export const CatButton = /*@__PURE__*/createReactComponent<JSX.CatButton, HTMLCa
 export const CatIcon = /*@__PURE__*/createReactComponent<JSX.CatIcon, HTMLCatIconElement>('cat-icon');
 export const CatInput = /*@__PURE__*/createReactComponent<JSX.CatInput, HTMLCatInputElement>('cat-input');
 export const CatMenu = /*@__PURE__*/createReactComponent<JSX.CatMenu, HTMLCatMenuElement>('cat-menu');
+export const CatRadio = /*@__PURE__*/createReactComponent<JSX.CatRadio, HTMLCatRadioElement>('cat-radio');
 export const CatScrollable = /*@__PURE__*/createReactComponent<JSX.CatScrollable, HTMLCatScrollableElement>('cat-scrollable');
 export const CatSkeleton = /*@__PURE__*/createReactComponent<JSX.CatSkeleton, HTMLCatSkeletonElement>('cat-skeleton');
 export const CatSpinner = /*@__PURE__*/createReactComponent<JSX.CatSpinner, HTMLCatSpinnerElement>('cat-spinner');
