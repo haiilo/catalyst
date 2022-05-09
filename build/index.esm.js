36c2263317f4685fa2c649ff5842567a39edbb6c
fynnfeldpausch <f.feldpausch@live.de>
2022-05-09 12:52:13 +0000

    Deploying to gh-pages from @ haiilo/catalyst@df4b64578a71bd058dc47e8ff3d1998b45953cd5 🚀

--- build/index.esm.js
+++ build/index.esm.js
@@ -1,2 +1,2 @@
-export{a as CatI18nRegistry,C as CatIconRegistry}from"./p-9a3c2375.js";
+export{a as CatI18nRegistry,C as CatIconRegistry}from"./p-2ec5c87a.js";export{N as NotificationsService}from"./p-c7f3b231.js";import"./p-7c06183a.js";
 //# sourceMappingURL=index.esm.js.map
