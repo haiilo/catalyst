15b95234064ef6394e60c05aab6ab981bc2f73c6
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2022-08-16 15:49:40 +0200

    chore: icons

--- core/src/index.cdn.js
+++ core/src/index.cdn.js
@@ -1,21 +1,43 @@
 (function (doc) {
-  var scriptElm = doc.scripts[doc.scripts.length - 1];
-  var warn = ['Discouraged script use, please remove: ' + scriptElm.outerHTML];
+  var scriptMap = doc.createElement('script');
+  scriptMap.setAttribute('type', 'importmap');
+  scriptMap.text = `
+    {
+      "imports": {
+        "@haiilo/catalyst": "https://cdn.jsdelivr.net/npm/@haiilo/catalyst/dist/catalyst/index.esm.js",
+        "@haiilo/catalyst-icons": "https://cdn.jsdelivr.net/npm/@haiilo/catalyst-icons/dist/js/icons.object.js"
+      }
+    }
+  `;
 
-  warn.push('To improve performance it is recommended to set the script in the head as follows:');
+  var scriptComp = doc.createElement('script');
+  scriptComp.setAttribute('type', 'module');
+  scriptComp.setAttribute('data-stencil-namespace', 'catalyst');
+  scriptComp.src = 'https://cdn.jsdelivr.net/npm/@haiilo/catalyst/dist/catalyst/catalyst.esm.js';
 
-  var parts = scriptElm.src.split('/');
-  parts.pop();
-  // add subfolder(s) here
-  // parts.push('...');
-  var url = parts.join('/');
+  var scriptInit = doc.createElement('script');
+  scriptInit.setAttribute('type', 'module');
+  scriptInit.text = `
+    import { ci } from '@haiilo/catalyst-icons';
+    import { CatI18nRegistry, CatIconRegistry } from '@haiilo/catalyst';
 
-  scriptElm = doc.createElement('script');
-  scriptElm.setAttribute('type', 'module');
-  scriptElm.src = url + '/catalyst.esm.js';
-  warn.push(scriptElm.outerHTML);
-  scriptElm.setAttribute('data-stencil-namespace', 'catalyst');
-  doc.head.appendChild(scriptElm);
+    CatIconRegistry.getInstance().addIcons(ci);
+    CatI18nRegistry.getInstance().register('en', {
+      'input.clear': 'Clear',
+      'input.optional': 'Optional',
+      'select.addItem': 'Add item',
+      'select.customAddItem': 'Custom add item',
+      'select.loading': 'Loading',
+      'select.noResults': 'No results',
+      'select.noChoices': 'No choices',
+      'select.removeItem': 'Remove item',
+      'select.searchPlaceholder': 'Search',
+      'select.selectItem': 'Select item',
+      'select.uniqueItem': 'Unique item'
+    });
+  `;
 
-  console.warn(warn.join('\n'));
+  doc.head.appendChild(scriptMap);
+  doc.head.appendChild(scriptComp);
+  doc.head.appendChild(scriptInit);
 })(document);
