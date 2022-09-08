52e435d4c62f14576fc0de3ebc80353eed6b660d
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2022-09-08 10:52:11 +0200

    chore: organize imports

--- core/src/index.ts
+++ core/src/index.ts
@@ -2,7 +2,5 @@ export { Components, JSX } from './components';
 export { CatI18nRegistry } from './components/cat-i18n/cat-i18n-registry';
 export { CatIconRegistry } from './components/cat-icon/cat-icon-registry';
 export { NotificationsService } from './components/cat-notification/cat-notification';
+export { CatSelectRemoteConnector, Item, Page, RenderInfo } from './components/cat-select-remote/cat-select-remote';
 export { CatSelectItem } from './components/cat-select/cat-select';
-export { CatSelectRemoteConnector } from './components/cat-select-remote/cat-select-remote';
-export { RenderInfo } from './components/cat-select-remote/cat-select-remote';
-export { Page } from './components/cat-select-remote/cat-select-remote';
