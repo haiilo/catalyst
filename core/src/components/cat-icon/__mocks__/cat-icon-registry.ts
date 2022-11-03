6b332173dc01f64c4053750d0cfbc9f8596aee02
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2022-11-03 11:51:03 +0100

    fix: prettier

--- core/src/components/cat-icon/__mocks__/cat-icon-registry.ts
+++ core/src/components/cat-icon/__mocks__/cat-icon-registry.ts
@@ -2,23 +2,23 @@ export class CatIconRegistry {
   static getInstance() {
     return new CatIconRegistry();
   }
-  
+
   getIcon(_name: string, _setName?: string): string | undefined {
     return undefined;
   }
-  
+
   addIcon(_name: string, _data: string, _setName?: string): CatIconRegistry {
     return this;
   }
-  
+
   addIcons(_icons: { [name: string]: string }, _setName?: string): CatIconRegistry {
     return this;
   }
-  
+
   removeIcon(_name: string, _setName?: string): CatIconRegistry {
     return this;
   }
-  
+
   removeIcons(_names: string[], _setName?: string): CatIconRegistry {
     return this;
   }
