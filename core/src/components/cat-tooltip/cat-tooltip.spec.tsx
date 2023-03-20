b4834e12b942bdf4258f3bcf6b3888109d5a7cbd
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2023-03-20 14:56:08 +0100

    fix(core): rework tooltip trigger selection (#189)

--- core/src/components/cat-tooltip/cat-tooltip.spec.tsx
+++ core/src/components/cat-tooltip/cat-tooltip.spec.tsx
@@ -14,9 +14,7 @@ describe('cat-tooltip', () => {
     expect(page.root).toEqualHtml(`
       <cat-tooltip content="This is a tooltip">
         <mock:shadow-root>
-          <div aria-describedby="cat-tooltip-0" class="tooltip-trigger" tabindex="0">
-            <slot></slot>
-          </div>
+          <slot></slot>
           <div class="tooltip tooltip-m" id="cat-tooltip-0">This is a tooltip</div>
         </mock:shadow-root>
         <p>Hover me</p>
