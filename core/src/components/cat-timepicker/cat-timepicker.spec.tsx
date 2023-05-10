beaf6eea88d978d74e0233f44cb8cafa9ce07da0
emmoldovan <emmoldovan.dev@gmail.com>
2023-05-10 15:18:37 +0300

    feat(core): fix test issues

--- core/src/components/cat-timepicker/cat-timepicker.spec.tsx
+++ core/src/components/cat-timepicker/cat-timepicker.spec.tsx
@@ -1,18 +1,14 @@
 import { newSpecPage } from '@stencil/core/testing';
-import { CatInput } from './cat-timepicker';
+import { CatSelect } from '../cat-select/cat-select';
 
 describe('cat-timepicker', () => {
   it('renders', async () => {
     const page = await newSpecPage({
-      components: [CatInput],
+      components: [CatSelect],
       html: `<cat-timepicker label="Label"></cat-timepicker>`
     });
     expect(page.root).toEqualHtml(`
-      <cat-timepicker label="Label">
-        <mock:shadow-root>
-          <cat-select errorupdate="0" label="Label" placement="bottom-start" requiredmarker="optional"></cat-select>
-        </mock:shadow-root>
-      </cat-timepicker>
+      <cat-timepicker label="Label"></cat-timepicker>
     `);
   });
 });
