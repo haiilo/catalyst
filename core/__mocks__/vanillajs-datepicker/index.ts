2fb2ddbed0bf2b651f48a201f0279e821b3b44cd
Emanuel Moldovan <emanuel.moldovan_ext@haiilo.com>
2023-06-01 16:10:59 +0300

    fix(core): fix prettier

--- core/__mocks__/vanillajs-datepicker/index.ts
+++ core/__mocks__/vanillajs-datepicker/index.ts
@@ -1,1 +1,1 @@
-export class Datepicker { }
+export class Datepicker {}
--- core/src/components/cat-label/cat-label.e2e.ts
+++ core/src/components/cat-label/cat-label.e2e.ts
@@ -2,7 +2,7 @@ import { newE2EPage } from '@stencil/core/testing';
 
 describe('cat-label', () => {
   beforeAll(() => (console.error = jest.fn()));
-  
+
   it('renders', async () => {
     const page = await newE2EPage();
     await page.setContent('<cat-label></cat-label>');
