a808d6e3cc7ab9e90662d00e19fbfc9ab4aec268
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2023-07-08 23:43:31 +0200

    chore: fix bug for other form elements

--- core/src/components/cat-select/cat-select.tsx
+++ core/src/components/cat-select/cat-select.tsx
@@ -540,14 +540,9 @@ export class CatSelect {
             'select-horizontal': this.horizontal
           }}
         >
-          <div
-            class={{
-              hidden: this.labelHidden,
-              'label-container': true
-            }}
-          >
+          <div class="label-container">
             {(this.hasSlottedLabel || this.label) && (
-              <label htmlFor={this.id}>
+              <label htmlFor={this.id} class={{ hidden: this.labelHidden }}>
                 <span class="label-wrapper" part="label">
                   {(this.hasSlottedLabel && <slot name="label"></slot>) || this.label}
                   <div class="label-metadata">
--- core/src/components/cat-textarea/cat-textarea.tsx
+++ core/src/components/cat-textarea/cat-textarea.tsx
@@ -216,12 +216,7 @@ export class CatTextarea {
             'textarea-horizontal': this.horizontal
           }}
         >
-          <div
-            class={{
-              hidden: this.labelHidden,
-              'label-container': true
-            }}
-          >
+          <div class="label-container">
             {(this.hasSlottedLabel || this.label) && (
               <label htmlFor={this.id} class={{ hidden: this.labelHidden }}>
                 <span class="label-wrapper" part="label">
