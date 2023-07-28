66b3138a1a00826489484d2b655566f1b6f41bf1
Emanuel Moldovan <emanuel.moldovan_ext@haiilo.com>
2023-07-28 13:22:52 +0300

    chore: run scripts and fix lint issues

--- angular/projects/catalyst/src/lib/directives/angular-component-lib/utils.ts
+++ angular/projects/catalyst/src/lib/directives/angular-component-lib/utils.ts
@@ -4,21 +4,21 @@ import { fromEvent } from 'rxjs';
 
 export const proxyInputs = (Cmp: any, inputs: string[]) => {
   const Prototype = Cmp.prototype;
-  inputs.forEach(item => {
+  inputs.forEach((item) => {
     Object.defineProperty(Prototype, item, {
       get() {
         return this.el[item];
       },
       set(val: any) {
         this.z.runOutsideAngular(() => (this.el[item] = val));
-      }
+      },
     });
   });
 };
 
 export const proxyMethods = (Cmp: any, methods: string[]) => {
   const Prototype = Cmp.prototype;
-  methods.forEach(methodName => {
+  methods.forEach((methodName) => {
     Prototype[methodName] = function () {
       const args = arguments;
       return this.z.runOutsideAngular(() => this.el[methodName].apply(this.el, args));
@@ -27,7 +27,7 @@ export const proxyMethods = (Cmp: any, methods: string[]) => {
 };
 
 export const proxyOutputs = (instance: any, el: any, events: string[]) => {
-  events.forEach(eventName => (instance[eventName] = fromEvent(el, eventName)));
+  events.forEach((eventName) => (instance[eventName] = fromEvent(el, eventName)));
 };
 
 export const defineCustomElement = (tagName: string, customElement: any) => {
