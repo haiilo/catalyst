742a8b05e2ca6b4e2b499185593ff103d28aedb1
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2023-09-08 23:06:12 +0200

    fix(core): datepicker uses incorrect formatting tokens

--- core/src/components/cat-datepicker/cat-datepicker.format.ts
+++ core/src/components/cat-datepicker/cat-datepicker.format.ts
@@ -1,7 +1,7 @@
 import { CatDatepickerMode } from './cat-datepicker.mode';
 
 export function getFormat(language: string, mode: CatDatepickerMode): string {
-  const date = new Date(2000, 1, 3, 4, 5, 6);
+  const date = new Date(2000, 1, 3, 16, 5, 6);
   const formatDate: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
   const formatTime: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit' };
   return mode === 'week'
@@ -20,7 +20,8 @@ export function getFormat(language: string, mode: CatDatepickerMode): string {
         .replace('00', 'y')
         .replace('02', 'm')
         .replace('03', 'd')
-        .replace('04', 'H')
+        .replace('04', 'h')
+        .replace('16', 'H')
         .replace('05', 'i')
         .replace(/AM|PM/i, 'K');
 }
