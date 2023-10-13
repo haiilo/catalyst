29254c246ea1b3c2bcdb0ae1f37316599a734151
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2023-10-13 12:18:06 +0200

    fix(angular): update errors on blur (closes #397)

--- angular/projects/catalyst/src/lib/directives/value-accessor-decorator.ts
+++ angular/projects/catalyst/src/lib/directives/value-accessor-decorator.ts
@@ -1,21 +1,30 @@
-import { AfterViewInit, Directive, ElementRef, Optional, Self } from '@angular/core';
-import { NgControl } from '@angular/forms';
+import { AfterViewInit, Directive, ElementRef, Host, Optional, Self, SkipSelf } from '@angular/core';
+import { ControlContainer, NgControl, Validators } from '@angular/forms';
 
 @Directive({
   /* tslint:disable-next-line:directive-selector */
-  selector: 'cat-input, cat-textarea, cat-datepicker, cat-select'
+  selector: 'cat-input, cat-textarea, cat-datepicker, cat-select',
+  host: {
+    '(catBlur)': 'updateErrors()'
+  }
 })
 export class ValueAccessorDecorator implements AfterViewInit {
   constructor(
     private readonly el: ElementRef,
-    @Self() @Optional() private readonly controlDirective?: NgControl
+    @Self() @Optional() private readonly controlDirective?: NgControl,
+    @Optional() @Host() @SkipSelf() private controlContainer?: ControlContainer
   ) {}
 
   ngAfterViewInit(): void {
     this.controlDirective?.statusChanges?.subscribe(() => this.updateErrors());
+    const controlName = this.controlDirective?.name?.toString() ?? '';
+    const control = this.controlContainer?.control?.get(controlName);
+    if (control?.hasValidator(Validators.required)) {
+      this.el.nativeElement.required = true;
+    }
   }
 
-  private updateErrors(): void {
+  updateErrors(): void {
     this.el.nativeElement.errors = this.controlDirective?.errors || !!this.controlDirective?.invalid;
   }
 }
