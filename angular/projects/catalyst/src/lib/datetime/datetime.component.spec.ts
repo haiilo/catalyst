dfc22b77ae628a81fc21d1a89f64ffe3011b711f
anastasiia_glushkova <anastasiia.glushkova@haiilo.com>
2025-09-23 22:19:51 +0200

    feat(angular): fix angular tests and add them to ci

--- angular/projects/catalyst/src/lib/datetime/datetime.component.spec.ts
+++ angular/projects/catalyst/src/lib/datetime/datetime.component.spec.ts
@@ -1,19 +1,29 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DatetimeComponent } from './datetime.component';
+import { DateValueAccessor } from '../directives/date-value-accessor';
 
 describe('DatetimeComponent', () => {
-  let component: DatetimeComponent;
-  let fixture: ComponentFixture<DatetimeComponent>;
+  @Component({
+    template: `
+      <cat-datetime>
+        <cat-date></cat-date>
+      </cat-datetime>
+    `
+  })
+  class TestComponent {}
+
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DatetimeComponent]
+      declarations: [DatetimeComponent, DateValueAccessor]
     })
-      .overrideTemplate(DatetimeComponent, '')
       .compileComponents();
 
-    fixture = TestBed.createComponent(DatetimeComponent);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- angular/projects/catalyst/src/lib/dialog/dialog.component.spec.ts
+++ angular/projects/catalyst/src/lib/dialog/dialog.component.spec.ts
@@ -1,13 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CatDialogComponent } from './dialog.component';
+import { DialogRef } from '@angular/cdk/dialog';
 
 describe('CatDialogComponent', () => {
   let component: CatDialogComponent;
   let fixture: ComponentFixture<CatDialogComponent>;
+  let dialogRef: jasmine.SpyObj<DialogRef>;
 
   beforeEach(async () => {
+    dialogRef = jasmine.createSpyObj('dialogRef', ['close']);
     await TestBed.configureTestingModule({
-      declarations: [CatDialogComponent]
+      declarations: [CatDialogComponent],
+      providers: [{
+        provide: DialogRef,
+        useValue: dialogRef
+      }]
     })
       .overrideTemplate(CatDialogComponent, '')
       .compileComponents();
