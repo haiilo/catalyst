3be4238df6fbdc2556f121ed92e0b3a5c2158878
Nora Simonow <nora.simonow@haiilo.com>
2025-10-29 10:43:14 +0100

    ci(angular): run tests for all 3 angular projects in CI

--- angular/projects/catalyst-demo/karma.conf.js
+++ angular/projects/catalyst-demo/karma.conf.js
@@ -34,7 +34,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
