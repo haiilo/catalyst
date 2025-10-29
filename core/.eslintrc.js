84ffa9db1373afdc15f958e5ba924a11bbf6046f
Philip Loesch <philiploesch@googlemail.com>
2025-10-29 18:14:52 +0100

    feat(core): upgrade to v9

--- core/.eslintrc.js
+++ core/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
--- core/.storybook/main.ts
+++ core/.storybook/main.ts
@@ -1,15 +1,22 @@
+import { createRequire } from 'node:module';
+import { dirname, join } from 'node:path';
 import type { StorybookConfig } from '@storybook/web-components-vite';
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('@storybook/addon-docs')
+  ],
+
   framework: {
-    name: '@storybook/web-components-vite',
+    name: getAbsolutePath('@storybook/web-components-vite'),
     options: {}
   },
-  docs: {
-    autodocs: 'tag'
-  },
+
   viteFinal: async (config, { configType }) => {
     // Only set base path for production (GitHub Pages subdirectory)
     if (configType === 'PRODUCTION') {
@@ -20,3 +27,7 @@ const config: StorybookConfig = {
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
