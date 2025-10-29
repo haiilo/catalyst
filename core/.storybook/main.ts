0dec787b236f3396d4ea4f25fa8d7fb1faa00a72
Philip Loesch <philiploesch@googlemail.com>
2025-10-29 14:59:33 +0100

    feat(core): continuing storybook

--- core/.storybook/main.ts
+++ core/.storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/web-components-vite',
     options: {}
