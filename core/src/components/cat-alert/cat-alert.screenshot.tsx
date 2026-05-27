55874cf90390c0ebb1d5dada3650c53282377cd3
anastasiia_glushkova <anastasiia.glushkova@haiilo.com>
2026-05-27 11:58:48 +0200

    test: update screenshot test

--- core/src/components/cat-alert/cat-alert.screenshot.tsx
+++ core/src/components/cat-alert/cat-alert.screenshot.tsx
@@ -6,7 +6,7 @@ describe('cat-alert screenshot', () => {
     const { root } = await render(
       <cat-alert color="success">
         <p>
-          Lorem ipsum dolor <a href="https://google.de">dolore magna</a> sit amet.
+          Lorem ipsum dolores <a href="https://google.de">dolore magna</a> sit amet.
         </p>
       </cat-alert>
     );
